9b4ada2afdb24ba1c8cb6cd1cd0280a40231ffa6
SamanSah <sahraei.saman91@gmail.com>
2024-03-25 03:49:26 +0330

    complete mapview and set new style for map

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="MapView">
+
+    <div 
+      id="BackBtn" 
+      class="absolute z-50 rounded-full bg-white p-1 top-8 left-4"
+      @click="goBack()"
+    >
+      <ArrowLeftIcon :size="40" />
+    </div>
+
     <div 
       id="map"
       class="w-full h-[45vh] top-0 left-0"
@@ -8,7 +17,7 @@
     <div id="VehicleSelection" class=" w-full">
       <div class="w-full h-2 border-t" />
       <div class="w-full text-center border-t-2 p-1.5 text-gray-700 text-lg font-semibold">
-        Distance - 50
+        Distance - {{ distance.text }}
       </div>
 
       <div class="scrollSection">
@@ -21,11 +30,11 @@
                 <div class="text-2xl mb-1">
                   UberX</div>
                 <div class="text-xl">
-                  £55.50
+                  £{{ calculatePrice(1, distance.value) }}
                 </div>
               </div>
               <div class="text-gray-500">
-                7 Hours
+                {{ duration.text }} Hours
               </div>
             </div>
           </div>
@@ -40,11 +49,11 @@
                   Comfort
                 </div>
                 <div class="text-xl">
-                  £55.50
+                  £{{ calculatePrice(1.25, distance.value) }}
                 </div>
               </div>
               <div class="text-gray-500">
-                7 Hours
+                {{ duration.text }} Hours
               </div>
             </div>
           </div>
@@ -59,11 +68,11 @@
                   UberXL
                 </div>
                 <div class="text-xl">
-                  £55.50
+                  £{{ calculatePrice(1.5, distance.value) }}
                 </div>
               </div>
               <div class="text-gray-500">
-                7 Hours
+                {{ duration.text }} Hours
               </div>
             </div>
           </div>
@@ -83,27 +92,102 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+  import axios from 'axios';
+  import { onMounted, ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  import ArrowLeftIcon from 'vue-material-design-icons/ArrowLeft.vue';
+  import { useDirectionStore } from '@/store/direction-store';
 
-onMounted(() => {
-  setTimeout(() => {
-    initMap()
-  }, 50);
-})
+  import mapStyles from '../mapStyles'
 
-const initMap = async () => {
-  const { Map } = await window.google.maps.importLibrary("maps");
+  const router = useRouter()
+  const direction = useDirectionStore()
 
-  const map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-    fullScreenControl: false,
-    zoomControl: false,
-    streetViewControl: false,
-    mapTypeControl: false
-  });
-  console.log('map.value', map);
-}
+  const distance = ref({text: '', value: null})
+  const duration = ref({text: '', value: null})
+  const latLng = ref({ start: { lat: null, lng: null }, end: { lat: null, lng: null } })
+
+  onMounted(async () => {
+    if (!direction.pickup || !direction.destination) { router.push('/') }
+    setTimeout(() => { initMap() }, 50)
+  })
+
+  const goBack = () => {
+    router.push('/directions')
+    direction.pickup = ''
+    direction.destination = ''
+  }
+
+  const initMap = () => {
+
+    const directionsService = new window.google.maps.DirectionsService()
+    const directionsRenderer = new window.google.maps.DirectionsRenderer()
+
+    directionsRenderer.setOptions({
+      polylineOptions: {
+        strokeColor: '#212121',
+        strokeWeight: 6
+      }
+    });
+
+    const map = new window.google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        minZoom: 3,
+        maxZoom: 17,
+        fullscreenControl: false,
+        zoomControl: false,
+        streetViewControl: false,
+        mapTypeControl: false,
+        
+        // https://snazzymaps.com/editor/customize/15
+        styles: mapStyles()
+      });
+
+      if (direction.pickup && direction.destination) { 
+        getDirections(map, directionsRenderer, directionsService)
+        getDistance()
+      }
+
+      return map
+  }
+
+  const getDirections = (map, directionsRenderer, directionsService) => {
+    
+    directionsRenderer.setMap(map)
+
+    const request = {
+      origin: direction.pickup,
+      destination: direction.destination,
+      optimizeWaypoints: true, // set to true if you want google to determine the shortest route or false to use the order specified.
+      travelMode: 'DRIVING'
+    }
+
+    directionsService.route(request, function (result, status) {
+      if (status === 'OK') {
+
+        latLng.value.start.lat = result.routes[0].legs[0].start_location.lat()
+        latLng.value.start.lng = result.routes[0].legs[0].start_location.lng()
+        latLng.value.end.lat = result.routes[0].legs[0].end_location.lat()
+        latLng.value.end.lng = result.routes[0].legs[0].end_location.lng()
+
+        directionsRenderer.setDirections(result)
+      }
+    })
+  }
+
+  const getDistance = async () => {
+    let res = await axios.get('distance/' + direction.pickup + '/' + direction.destination)
+
+    distance.value.text = res.data.rows[0].elements[0].distance.text
+    distance.value.value = res.data.rows[0].elements[0].distance.value
+    duration.value.text = res.data.rows[0].elements[0].duration.text
+    duration.value.value = res.data.rows[0].elements[0].duration.value
+  }
+
+  const calculatePrice = (multiplier, price) => {
+    let res = (price / 900) * multiplier
+    return res.toFixed(2)
+  }
 </script>
 
 <style lang="sass">
